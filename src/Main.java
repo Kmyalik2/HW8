686d492e06a11ca450bd1ab5a7d13804b201727a
Ksenia <kmyalik2@gmail.com>
2022-09-12 22:11:32 +0300

    HW8 files committed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person person1 = new Person("Ksenia", "Myalik", Gender.FEMALE);
+        Person person2 = new Person("Ksenia", "Myalik",Gender.FEMALE);
+        Person person3 = new Person("Andrew", "Unknown",Gender.MALE);
+        Person person4 = new Person("Kate", "White",Gender.FEMALE);
+        Person person5 = new Person("Andrew", "Unknown",Gender.MALE);
+
+        List<Person> peopleList = new ArrayList();
+        peopleList.add(0,person1);
+        peopleList.add(1, person2);
+        peopleList.add(2, person3);
+        peopleList.add(3, person4);
+        peopleList.add(4, person5);
+
+        System.out.println("Array List");
+        for (int i=0; i<peopleList.size(); i++) {
+            System.out.println(peopleList.get(i));
+        }
+        System.out.println("Array List size: " + peopleList.size() + "\n");
+
+        Set<Person> peopleSet = new HashSet<>(peopleList);
+        System.out.println("Hash Set");
+        Iterator itr = peopleSet.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        System.out.println("Hash Set size: " + peopleSet.size() + "\n");
+
+        Map<Integer, Person> peopleHashmap = new HashMap<>();
+        peopleHashmap.put(1,person1);
+        peopleHashmap.put(2,person2);
+        peopleHashmap.put(3,person3);
+        peopleHashmap.put(4,person4);
+        peopleHashmap.put(5,person5);
+        //System.out.println("HashMap: " + peopleHashmap);
+        for (Integer key : peopleHashmap.keySet())
+            System.out.println("Key = " + key + ", " + peopleHashmap.get(key));
+        System.out.println("HashMap Size: " + peopleHashmap.size());
     }
+}
+
+
+enum Gender {
+    MALE,
+    FEMALE,
 }
---
+++ src/Person.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+
+public class Person {
+    final private String firstName;
+    final private String lastName;
+    final private Gender gender;
+
+
+    public Person(String firstName, String lastName, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return firstName.equals(person.firstName) && lastName.equals(person.lastName) && gender == person.gender;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, gender);
+    }
+
+    @Override
+    public String toString() {
+        return lastName.toUpperCase() + " " + firstName + ", " + gender + ";";
+    }
+
+}
